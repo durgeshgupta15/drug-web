71c3f859f06ba25f23fc5a2cda6dc17ad199adde
deedurgesh <durgesh.gupta@axestrack.com>
2021-03-16 17:41:14 +0530

    login page created

--- src/app/basic-components/login-page/login-page.page.ts
+++ src/app/basic-components/login-page/login-page.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPagePage implements OnInit {
 
-  constructor() { }
+  submitted: boolean = false;
+  signupForm: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder,
+    public navCtrl: NavController,
+    public router: Router) {
+    this.signupForm = this.formBuilder.group({
+      user_name: ['', [Validators.required]],
+      user_email: ['', [Validators.required, Validators.email]],
+      user_pwd: ['', [Validators.required, Validators.minLength(6)]],
+      user_rePwd: ['', [Validators.required]],
+    })
+   }
 
   ngOnInit() {
   }
+
+  get f() { return this.signupForm.controls; }
+
+  onSubmit(){
+    console.log('form is submitted');
+    console.log('data is: ', this.f);
+    console.log(this.signupForm.value);
+    this.submitted = true;
+    if(this.signupForm.invalid){
+      return;
+    }
+    this.navCtrl.navigateForward('/home')
+  }
+  
 
 }

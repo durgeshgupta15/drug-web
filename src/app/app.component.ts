2b9b426ddea48c6011b6b04b15376d4f0514310b
deedurgesh <durgesh.gupta@axestrack.com>
2021-03-18 19:31:21 +0530

    home page ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+
+interface menu {
+  name: string;
+  icon: string;
+  width?: string;
+  action?: string;
+  route?: string;
+  show?: boolean;
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  menuColor;
+  menus: menu[] = [
+    { name: 'Dashboard', icon: 'home', route: '/vehicle-list' },
+    { name: 'Share', icon: 'share-alt', action: 'reachUs' },
+    { name: 'About Us', icon: 'download', action: 'appDownload' },
+    { name: 'Payment', icon: 'rupee-sign', route: '/cf-payment' },
+    { name: 'Logout', icon: 'sign-out-alt', action: 'logout' },
+  ]
+
+
+  constructor(private menuCtrl: MenuController,
+    private navCtrl: NavController) {}
+
+  menuAction(menu: any) {
+    console.log('menuAction:')
+    this.menuCtrl.toggle();
+
+    if (menu.action) {
+      this[menu.action]();
+      return;
+    }
+    this.navCtrl.navigateRoot(menu.route);
+  }
 }
